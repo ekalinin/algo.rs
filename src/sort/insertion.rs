643679414fe92c273e7b418b38f0d97275d8c63a
Eugene Kalinin <e.v.kalinin@gmail.com>
2017-01-24 17:19:02 +0300

    added insertion sort by cormen; + prettified some output

--- src/sort/insertion.rs
+++ src/sort/insertion.rs
@@ -1,16 +1,39 @@
 use std::fmt;
 
+// Sedgewick edition (~30% faster than Cormen's one)
+// p.251
 pub fn sort<T: Ord + fmt::Debug>(input: &mut Vec<T>) {
     for i in 1..input.len() {
         'back: for j in (1..(i+1)).rev() {  // as right border is not included, do: i+1
-            if input[j] < input[j-1] {
-                input.swap(j, j-1);
+            if input[j-1] > input[j] {
+                input.swap(j-1, j);
             } else {
                 break 'back;
             }
         }
     }
 }
+
+//use std::clone;
+// Cormen edition
+// p.18
+//pub fn sort<T: Ord + fmt::Debug + clone::Clone>(input: &mut Vec<T>) {
+//    for j in 1..input.len() {
+//        let key = input[j].clone();
+//        let mut i = j - 1;
+//        let mut insert_first = false;
+//        'back: while input[i] > key {
+//            input[i + 1] = input[i].clone();
+//            if i == 0 {
+//                insert_first = true;
+//                break 'back;
+//            } else {
+//                i = i - 1;
+//            }
+//        }
+//        input[if insert_first {0} else {i + 1}] = key;
+//    }
+//}
 
 
 #[cfg(test)]
@@ -22,9 +45,9 @@ mod tests {
         let mut tests = super::super::get_test_vecs();
         for t in tests.iter_mut(){
             let test_slice = t.as_mut();
-            println!("= Unsorted: {:?}", test_slice);
+            println!("+ Unsorted: {:?}", test_slice);
             sort(test_slice);
-            println!("-  Sorted:   {:?}", test_slice);
+            println!("-   Sorted: {:?}", test_slice);
             assert!(super::super::is_sorted(test_slice));
         }
     }
